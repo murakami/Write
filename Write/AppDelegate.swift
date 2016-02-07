236acd0079e530a0c2099744a4581a0794626a21
村上幸雄 <yukio@mbpci7.local>
2016-02-07 23:25:12 +0900

    init

---
+++ Write/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Write
+//
+//  Created by 村上幸雄 on 2016/02/07.
+//  Copyright © 2016年 MURAKAMI Yukio. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Write/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Write
+//
+//  Created by 村上幸雄 on 2016/02/07.
+//  Copyright © 2016年 MURAKAMI Yukio. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
